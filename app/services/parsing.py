33dfc1e440d96edb275df1f7d9afe865cda11556
nickschu <nickschu@umich.edu>
2024-12-29 19:55:57 -0500

    impl. basic parse/chunk + begin embedding impl.

--- app/services/parsing.py
+++ app/services/parsing.py
@@ -1,1 +1,20 @@
-# TODO: Parse pdf into raw text and chunk text into sections
+import pdfplumber
+import tiktoken
+
+def num_tokens_from_string(string: str, encoding_name: str = "cl100k_base") -> int:
+    """Returns the number of tokens in a text string."""
+    encoding = tiktoken.get_encoding(encoding_name)
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+
+# TODO: This function should be more sophisticated. Instead of chunking by page, should be by section
+#       determined by extracted document formatting.
+def chunk_text_from_pdf(file_path: str) -> str:
+    text_chunks = []
+    with pdfplumber.open(file_path) as pdf:
+        for page in pdf.pages:
+            text = page.extract_text()
+            if text:
+                # TODO: Check embedding size limits via tiktoken. OAI embeddings allow up to 8191 tokens.
+                text_chunks.append(text)
+    return text_chunks
